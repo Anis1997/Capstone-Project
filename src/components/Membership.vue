f7086b89920536a2b742eb3bcf9448a3b516a410
Anis Amin <anismohdamin@yahoo.com>
2019-11-19 16:13:40 +0900

    Updated on 19th November

--- src/components/Membership.vue
+++ src/components/Membership.vue
@@ -234,23 +234,14 @@ export default {
           }, ]
         }
       })
-      .then((response) => {
-        console.log(response);
-        this.buying_manager = response.data.docs[0]._source.name;
-        this.buying_store = response.data.docs[0]._source.address;
-      }).catch((e) => {
-        console.log(e.response)
-      })
-    var currentDate = new Date();
-    var currentDateWithFormat = new Date().toJSON().slice(0, 10).replace(/-/g, '-');
-    this.buying_date = currentDateWithFormat;
+
   },
   destroyed() {
       this.$barcodeScanner.destroy()
   },
   methods: {
     goto_home() {
-      this.$router.replace('/home')
+      this.$router.replace('/')
     },
     openDialogFunction(){
       payWindow = true;
@@ -259,180 +250,8 @@ export default {
     closeDialogFunction(){
       payWindow = false;
     },
-    check_coin(){
-      if(this.user_barcode == null){
-         //test용 테스트시 alert 부분 주석처리하고 이거 사용할것
-        //this.user_barcode = "8801111000000"
-        alert("바코드를 입력해주세요.")
-      }
-      if(payWindow == true && this.user_barcode != null){
-        axios({
-            method: 'POST',
-            url : baseurl+'/bakery_customer/_search',
-            headers: {
-                Authorization: 'Basic anhaMFFSQzdhOjhlNDhjYzhlLWUxNmUtNDNiNy1hZjUyLTkzODBkZmU1NDVhNA==',
-                'Content-Type': 'application/json'
-            },
-            data: {
-                  'query':{
-                      'match_phrase':{
-                          'barcode': this.user_barcode
-                      }
-                  }
-               }
-        }).then((response) => {
-            console.log(response);
-            if(response.data.hits.hits.legnth != 0 ){
-              var data = response.data.hits.hits[0]._source
-              var coin = data.bakeryCoin
-              this.user_name = data.name
-              this.user_id = response.data.hits.hits[0]._id
-              if(this.tot_price <= coin){
-                this.user_remain_coin = coin*1 - this.tot_price*1
-                alert("결제 가능합니다.\n현재 Bakery Coin 잔액 : "+numberFormat(coin)+"원\n결제 후 Bakery Coin 잔액 : "+numberFormat(this.user_remain_coin)+"원")
-                this.user_bakeryCoin = this.tot_price
-                document.getElementById("payButton").disabled = false
-                this.ok_pressed = true
-              }
-              else{
-                alert("잔액이 부족합니다.\n현재 Bakery Coin 잔액 : "+numberFormat(coin)+"원")
-              }
-            }
-            else {
-              alert("회원 정보에 해당 바코드가 존재하지 않습니다.\n회원이 아니시거나 바코드가 잘못되었습니다.")
-            }
-        }).catch((ex)=> {
-            console.log("ERR!!!!! : ", ex)
-        })
-      }
-    },
-    defaultQuery: function(value, props){
-      return {
-        query: {
-          match: {
-            pManagerID: this.uid
-          }
-        }
-      }
-    },
-    onBarcodeScanned (barcode) {
-      console.log(barcode)
-      if(payWindow == true){ // 결제창일때 바코드 작동
-        document.getElementById("user_barcode").value = barcode // 바코드란 값 변경해주기
-        this.user_barcode = barcode;
-      }
-      else{ //결제창 아닐때 바코드 작동
-        axios({
-          method: 'GET',
-          url : baseurl+'/bakery_product/_search?q=*'+barcode+'*',
-          headers: {
-            Authorization: 'Basic cnVjeHhkam0zOjdkNWZkM2I2LWYyMzctNGMzMS1hZDJiLTVhYjVmZjNiM2FlMg=='
-          }
-        }).then((response) => {
-            console.log(response);
-            var pName = ''
-            var pPrice = ''
-            pName = response.data.hits.hits[0]._source.pName
-            pPrice = response.data.hits.hits[0]._source.pPrice
-            if (this.product_list.includes(pName)) {
-              alert("이미 목록에 존재하는 상품입니다.")
-            } else {
-              this.product_list.push(pName)
-              this.product_amount.push(1)
-              this.sum += parseInt(pPrice)
-            }
-            var html = '<table>';
-            for (var i = 0; i < this.product_list.length; i++) {
-              html += '<tr>';
-              html += '<td>' + this.product_list[i] + '</td>';
-              html += '<td>' + this.product_amount[i] + '</td>';
-              html += '</tr>';
-            }
-            html += '</table>';
-            document.getElementById("lists").innerHTML = html;
-        }).catch((ex)=> {
-            alert("바코드에 해당되는 상품이 없습니다!")
-            console.log("ERR!!!!! : ", ex)
-        })
-      }
-    },
-    get_name(name, price, id) {
-      this.product_name = name
-      if (this.product_list.includes(name)) {
-        alert("이미 품목에 추가되었습니다")
-      } else {
-        this.product_list.push(name)
-        this.product_amount.push(1)
-        this.product_id.push(id)
-        this.sum += parseInt(price)
-      }
-      var html = '<table>';
-      for (var i = 0; i < this.product_list.length; i++) {
-        html += '<tr>';
-        html += '<td>' + this.product_list[i] + '</td>';
-        html += '<td>' + this.product_amount[i] + '</td>';
-        html += '</tr>';
-      }
-      html += '</table>';
-      document.getElementById("lists").innerHTML = html;
-    },
-    reset() {
-      this.product_list = []
-      this.product_amount = []
-      this.product_id = []
-      this.sum = 0
-      this.tot_price = 0
-      document.getElementById("lists").innerHTML = this.product_list
-    },
-    delete_product(name, price) {
-      var a = this.product_list.indexOf(name);
-      if (a != -1) {
-        var temp = (this.product_amount[a]) * parseInt(price);
-        this.product_list.splice(a, 1);
-        this.product_amount.splice(a, 1);
-        this.product_id.splice(a, 1);
-        this.sum -= temp;
-        var html = '<table>';
-        for (var i = 0; i < this.product_list.length; i++) {
-          html += '<tr>';
-          html += '<td>' + this.product_list[i] + '</td>';
-          html += '<td>' + this.product_amount[i] + '</td>';
-          html += '</tr>';
-        }
-        html += '</table>';
-        document.getElementById("lists").innerHTML = html;
-      }
-    },
-    change_amount(name, price) {
-      var a = this.product_list.indexOf(name);
-      if (a != -1) {
-        if (this.user_amount > 0) {
-          if (this.user_amount > this.product_amount[a]) {
-            // add (input-original)
-            this.sum += (this.user_amount - this.product_amount[a]) * parseInt(price);
-          } else if (this.user_amount < this.product_amount[a]) {
-            // gap
-            this.sum -= (this.product_amount[a] - this.user_amount) * parseInt(price);
-          }
-          this.product_amount[a] = this.user_amount;
-          this.user_amount = null;
-          var html = '<table>';
-          for (var i = 0; i < this.product_list.length; i++) {
-            html += '<tr>';
-            html += '<td>' + this.product_list[i] + '</td>';
-            html += '<td>' + this.product_amount[i] + '</td>';
-            html += '</tr>';
-          }
-          html += '</table>';
-          document.getElementById("lists").innerHTML = html;
-          this.is_clicked = false;
-        } else {
-          alert("숫자를 입력하세요!");
-        }
-      } else {
-        alert("메뉴를 먼저 선택하세요!");
-      }
-    },
+    
+   
     pay() {
       if (this.ok_pressed) {
         this.remain = null;
@@ -447,26 +266,7 @@ export default {
           this.user_name = '익명';
         }
         // set rDetail: rAmount, rName
-        var user_detail = '[';
-        var length = this.product_list.length;
-        for (var i = 0; i < length; i++) {
-          user_detail += '{ "rAmount" : "';
-          user_detail += this.product_amount[i];
-          user_detail += '", "rName" : "';
-          user_detail += this.product_list[i];
-          if (i != length - 1) {
-            user_detail += '" },';
-          } else {
-            user_detail += '" }]';
-          }
-        }
-        // set rType: cash, coin
-        var user_type = '{ "cash" : "';
-        user_type += this.user_money;
-        user_type += '", "coin" : "';
-        user_type += this.user_bakeryCoin;
-        user_type += '" }';
-        if(this.user_barcode != null && this.bakeryCoin != 0){
+       
           axios({
               method: 'POST',
               url: baseurl + '/bakery_customer/_doc/' + this.user_id + '/_update',
@@ -482,55 +282,15 @@ export default {
             })
             .then((response) => {
               //var hits_length = response.data.hits.hits.length
-              console.log("바코드 유저 결제 완료")
+              console.log("")
               console.log(response);
             }).catch((e) => {
               console.log(e.response)
             })
         }
-        //axios POST 레코드 생성
-        axios({
-            method: 'POST',
-            url: baseurl + '/bakery_record/_doc',
-            headers: {
-              Authorization: 'Basic VHF6Z0M3cHhTOjIyMmQyNTE3LTIzNzUtNDMyNC1iZjc1LTEzMDg1ZWM0YWE3ZA==',
-              'Content-Type': 'application/json'
-            },
-            data: {
-              "rStore": this.buying_store,
-              "rDate": this.buying_date,
-              "rBuyer": this.user_name,
-              "rPrice": this.sum,
-              "rDetail": JSON.parse(user_detail),
-              "rType": JSON.parse(user_type),
-              "rManagerID": this.uid,
-            }
-          })
-          .then((response) => {
-            console.log(response);
-            alert("record DB에 입력 완료!");
-            alert("결제가 성공적으로 진행되었습니다");
-            window.history.go(0);
-          }).catch((e) => {
-            console.log(e.response)
-          })
-      } else {
-        alert("에러: 승인 거부");
-      }
+
     },
-    onCancel() {},
-    edit_clicked(name) {
-      var a = this.product_list.indexOf(name);
-      if (a != -1) {
-        this.now_product_name = name;
-        this.is_clicked = true;
-      } else {
-        alert("메뉴를 먼저 선택하세요!");
-      }
-    },
-    get_now_name() {
-      return (this.now_product_name);
-    },
+ 
     got_money() {
       // when using CASH
       this.remain = null;
@@ -550,18 +310,7 @@ export default {
         alert("에러: 받은 현금이 없습니다!");
       }
     },
-    close_pay() {
-      this.remain = null;
-      this.user_money = null;
-      this.card_number = null;
-      this.showDialog = false;
-      this.tot_price = 0;
-      this.product_list = [];
-      this.product_amount = [];
-      this.product_price = [];
-      this.product_id = [];
-      document.getElementById("lists").innerHTML = this.product_list
-    },
+    
   }
 }
 </script>
